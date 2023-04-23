140979493fe69c73d8b3c623aa369600e7c4ac8c
logan1x <sharmakhushal78@gmail.com>
2023-04-23 08:40:05 +0530

    reaf: homepage changes for lighthouse and del manifest

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import { motion } from "framer-motion";
 
 function App() {
   return (
-    <motion.div
-      className="App"
-      initial={{
-        opacity: 0,
-        x: -100,
-      }}
-      animate={{
-        opacity: 1,
-        x: 0,
-      }}
-      transition={{
-        duration: 1,
-        delay: 0.5,
-      }}
-    >
-      <h1>Hi, Welcome to the talk.</h1>
+    <motion.div className="App">
+      <motion.h1
+        initial={{
+          opacity: 0,
+          x: -100,
+        }}
+        animate={{
+          opacity: 1,
+          x: 0,
+        }}
+        transition={{
+          duration: 1,
+          delay: 0.5,
+        }}
+      >
+        Hi, Welcome to the talk.
+      </motion.h1>
       <h2>Let's pray to demo gods!</h2>
     </motion.div>
   );
